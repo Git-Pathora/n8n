e50f60fab5d862320575b529bd67d6d8b82bbad5
Elias Meire <elias@meire.dev>
2026-02-05 08:42:22 +0100

    Add QuickConnectSignInButton component

---
+++ packages/frontend/editor-ui/src/features/credentials/components/QuickConnectSignInButton.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { N8nButton } from '@n8n/design-system';
+import CredentialIcon from './CredentialIcon.vue';
+import GoogleAuthButton from './CredentialEdit/GoogleAuthButton.vue';
+
+defineProps<{
+	credentialTypeName: string;
+	buttonText: string;
+	isGoogle: boolean;
+}>();
+
+defineEmits<{
+	click: [];
+}>();
+</script>
+
+<template>
+	<GoogleAuthButton v-if="isGoogle" @click="$emit('click')" />
+	<N8nButton
+		v-else
+		size="large"
+		type="secondary"
+		:class="$style.signInButton"
+		@click="$emit('click')"
+	>
+		<CredentialIcon :credential-type-name="credentialTypeName" :size="20" />
+		<span :class="$style.buttonText">{{ buttonText }}</span>
+	</N8nButton>
+</template>
+
+<style lang="scss" module>
+.signInButton {
+	display: inline-flex;
+	align-items: center;
+	gap: var(--spacing--2xs);
+}
+
+.buttonText {
+	margin-left: var(--spacing--3xs);
+}
+</style>
